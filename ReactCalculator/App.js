b1a9bda73b0f353d1f003e2d0a823670bb76ed2e
Matthew Walker <matsaleh13@gmail.com>
2018-02-11 12:24:35 -0600

    WIP tutorial
    https://facebook.github.io/react-native/docs/tutorial.html

---
+++ ReactCalculator/App.js
@@ -0,0 +1,43 @@
+// Generated
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <Text>Changes you make will automatically reload.</Text>
+//         <Text>Shake your phone to open the developer menu.</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+
+// From tutorial
+import React, { Component } from 'react';
+import {
+  Text,
+  AppRegistry
+} from 'react-native';
+
+export default class ReactCalculator extends Component {
+  render() {
+    return (
+      <Text>Hello, React!</Text>
+    )
+  }
+}
+
+// AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator);
